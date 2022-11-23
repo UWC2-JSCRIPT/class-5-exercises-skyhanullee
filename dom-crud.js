d35f8fca17b2109bcdb5a8afa8f614e54af50f77
skyhanullee <leeskyha@gmail.com>
2022-11-22 23:26:37 -0800

    answered some of the questions

--- dom-crud.js
+++ dom-crud.js
@@ -1,6 +1,14 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
+const aBuyNow = document.createElement('a');
+// const aBuyNowText = document.createTextNode('Buy Now!')
+// aBuyNow.appendChild(aBuyNowText);
+aBuyNow.textContent('Buy Now!');
+aBuyNow.setAttribute('id', 'cta')
 
+
+const lastP = document.getElementsByTagName('p');
+lastP.appendChild(aBuyNow);
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
