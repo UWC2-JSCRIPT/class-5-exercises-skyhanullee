a3a55d0f48acdf0664206252da6721c75efcca31
skyhanullee <48109897+skyhanullee@users.noreply.github.com>
2022-11-23 05:38:38 -0800

    completed the exercises

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,19 @@
+let counter = 0;
+
 // When a user clicks the + element, the count should increase by 1 on screen.
+const plusEl = document.getElementById('plus');
+
+plusEl.addEventListener('click', (e) => {
+    counter++;
+    // console.log(counter);
+    document.getElementById('counter').innerHTML = counter;
+});
+
+
 // When a user clicks the – element, the count should decrease by 1 on screen.
+const minusEl = document.getElementById('minus');
+
+minusEl.addEventListener('click', (e) => {
+    counter--;
+    document.getElementById('counter').innerHTML = counter;
+});
